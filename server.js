ff4f0a86f776fef4c0ec34a43859224c35ab46c9
githagman <thomas@thomashagman.com>
2013-07-20 14:26:14 +0000

    initial server commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require("http");
+
+function start() {
+	function onRequest(req, res) {
+		console.log("Request received");
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.write("Hello world");
+		res.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server started");
+}
+
+exports.start = start;
